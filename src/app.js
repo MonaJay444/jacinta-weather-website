f74cdda54cf7bf258438446a38ffbdd02d61a4ee
Ukauwa Jacinta <monalisajacinta@gmail.com>
2019-06-11 16:02:16 -0800

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+
 const app = express()
+
 const port = process.env.PORT || 3000
 
 // Define paths for Express config
